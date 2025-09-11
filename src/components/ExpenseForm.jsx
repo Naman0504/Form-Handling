fabc1f25b6463924228b085e7ebbeda800cac80f
Naman0504 <matiyaranaman@gmail.com>
2025-09-11 17:21:22 +0530

    handling data using different state variable

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,37 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ExpenseForm = ({setExpenses}) => {
+const ExpenseForm = ({ setExpenses }) => {
 
-    const handleSubmit = (e)=>{
-               e.preventDefault();
-               getFromData(e.target);
-               const expense = {...getFromData(e.target),id:crypto.randomUUID()}
-               setExpenses((prevState)=> [...prevState,expense])
-               
+    const [title, setTitle] = useState('')
+    const [category, setCategory] = useState('')
+    const [amount, setAmount] = useState('')
 
-               e.target.reset()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const expense = {title,category,amount,id:crypto.randomUUID()}
+        setExpenses((prevState)=>[...prevState,expense]) 
+        
+
+        setAmount('');
+        setCategory('');
+        setTitle('')
+
     }
 
-    const getFromData = (form) =>{
-            const formData = new FormData(form)
-               const data ={}     
-            for(const [key,value] of formData.entries()){
-                console.log(key,value);   
-                data[key] = value; 
-            }
+    // const handleSubmit = (e)=>{
+    //            e.preventDefault();
+    //            getFromData(e.target);
+    //            const expense = {...getFromData(e.target),id:crypto.randomUUID()}
+    //            setExpenses((prevState)=> [...prevState,expense])
 
-        return data;
-    }
+
+    //            e.target.reset()
+    // }
+
+    // const getFromData = (form) =>{
+    //         const formData = new FormData(form)
+    //            const data ={}     
+    //         for(const [key,value] of formData.entries()){
+    //             console.log(key,value);   
+    //             data[key] = value; 
+    //         }
+
+    //     return data;
+    // }
+
+    // console.log("Rendering....")
 
     return (
         <form className="expense-form" onSubmit={handleSubmit}>
             <div className="input-container">
                 <label htmlFor="title">Title</label>
-                <input id="title" name="title" />
+                <input id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)} />
             </div>
             <div className="input-container">
                 <label htmlFor="category">Category</label>
-                <select id="category" name="category">
+                <select id="category" name="category" value={category} onChange={(e) => setCategory(e.target.value)}>
                     <option disabled="true" hidden>select category</option>
                     <option value="grocery">Grocery</option>
                     <option value="clothes">Clothes</option>
@@ -42,7 +60,7 @@ const ExpenseForm = ({setExpenses}) => {
             </div>
             <div className="input-container">
                 <label htmlFor="amount">Amount</label>
-                <input id="amount" name="amount"/>
+                <input id="amount" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
             </div>
             <button className="add-btn">Add</button>
         </form>
