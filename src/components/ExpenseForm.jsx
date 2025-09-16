d2a301296037fa552e74d11999ea37ca4f303112
Naman0504 <matiyaranaman@gmail.com>
2025-09-16 16:18:21 +0530

    Validate Form errors

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -5,6 +5,7 @@ const ExpenseForm = ({ setExpenses }) => {
     // const [title, setTitle] = useState('')
     // const [category, setCategory] = useState('')
     // const [amount, setAmount] = useState('')
+    const [errors, setErrors] = useState({})
 
     const [expense, setExpense] = useState({
         id: crypto.randomUUID(),
@@ -13,10 +14,35 @@ const ExpenseForm = ({ setExpenses }) => {
         amount: ''
     })
 
+
+    //validate
+    const validate = (formData) => {
+
+        console.log("Form", formData)
+        const errorsData = {}
+        if(!formData.title){
+            errorsData.title = "Title is required";
+        }
+        if(!formData.category){
+            errorsData.category = "Category is required";
+        }
+        if(!formData.amount){
+            errorsData.amount = "amount is required";
+        }
+        
+        setErrors(errorsData)
+        return errorsData;
+
+    }
+
     //handling data using different state variable
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(expense)
+
+        const validateResult = validate(expense);
+        console.log("Validation",validateResult)
+        if(Object.keys(validateResult).length) return;
+
         setExpenses((prevState) => [...prevState, { ...expense, id: crypto.randomUUID() }])
 
         setExpense({
@@ -64,15 +90,23 @@ const ExpenseForm = ({ setExpenses }) => {
 
     // console.log("Rendering....")
 
+    const handleChange = (e) => {
+        // console.log(e.target)
+        const { name, value } = e.target;
+        setExpense((prevState) => ({ ...prevState, [name]: value }))
+    }
+
     return (
         <form className="expense-form" onSubmit={handleSubmit}>
             <div className="input-container">
                 <label htmlFor="title">Title</label>
-                <input id="title" name="title" value={expense.title} onChange={(e) => setExpense((prevState) => ({ ...prevState, title: e.target.value }))} />
+                <input id="title" name="title" value={expense.title} onChange={handleChange} />
+                <p className='error-tag'>{errors.title}</p>
+
             </div>
             <div className="input-container">
                 <label htmlFor="category">Category</label>
-                <select id="category" name="category" value={expense.category} onChange={(e) => setExpense((prevState) => ({ ...prevState, category: e.target.value }))}>
+                <select id="category" name="category" value={expense.category} onChange={handleChange}>
                     <option disabled="true" hidden>select category</option>
                     <option value="grocery">Grocery</option>
                     <option value="clothes">Clothes</option>
@@ -80,10 +114,12 @@ const ExpenseForm = ({ setExpenses }) => {
                     <option value="education">Education</option>
                     <option value="medicine">Medicine</option>
                 </select>
+                <p className='error-tag'>{errors.category}</p>
             </div>
             <div className="input-container">
                 <label htmlFor="amount">Amount</label>
-                <input id="amount" name="amount" value={expense.amount} onChange={(e) => setExpense((prevState) => ({ ...prevState, amount: e.target.value }))} />
+                <input id="amount" name="amount" value={expense.amount} onChange={handleChange} />
+                <p className='error-tag'>{errors.amount}</p>
             </div>
             <button className="add-btn">Add</button>
         </form>
